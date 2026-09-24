7c7fce1324c9e834db266cd99b3fbfa6babcc259
Redger Xu <redgerxu@gmail.com>
2026-09-23 17:25:11 -0700

    added nh, updated hyprland layout

--- configuration.nix
+++ configuration.nix
@@ -81,6 +81,11 @@
   programs.firefox.enable = true;
   programs.hyprland.enable = true;
   programs.zsh.enable = true;
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+  };
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -2,24 +2,21 @@
   wayland.windowManager.hyprland = {
     enable = true;
     configType = "lua";
-    extraConfig = "";
+    extraConfig = builtins.readFile ../hypr/hyprland.lua;
+
+    extraLuaFiles = {
+      "conf/env.lua" = ../hypr/conf/env.lua;
+      "conf/input.lua" = ../hypr/conf/input.lua;
+      "conf/programs.lua" = ../hypr/conf/programs.lua;
+      "conf/monitors.lua" = ../hypr/conf/monitors.lua;
+      "conf/appearance.lua" = ../hypr/conf/appearance.lua;
+      "conf/keybinds.lua" = ../hypr/conf/keybinds.lua;
+      "conf/plugins.lua" = ../hypr/conf/plugins.lua;
+      "conf/autostart.lua" = ../hypr/conf/autostart.lua;
+    };
   };
 
   xdg.configFile = {
-    # Entry point
-    "hypr/hyprland.lua".source = ../hypr/hyprland.lua;
-
-    # Config modules
-    "hypr/conf/env.lua".source = ../hypr/conf/env.lua;
-    "hypr/conf/input.lua".source = ../hypr/conf/input.lua;
-    "hypr/conf/programs.lua".source = ../hypr/conf/programs.lua;
-    "hypr/conf/monitors.lua".source = ../hypr/conf/monitors.lua;
-    "hypr/conf/appearance.lua".source = ../hypr/conf/appearance.lua;
-    "hypr/conf/keybinds.lua".source = ../hypr/conf/keybinds.lua;
-    "hypr/conf/plugins.lua".source = ../hypr/conf/plugins.lua;
-    "hypr/conf/autostart.lua".source = ../hypr/conf/autostart.lua;
-
-    # Companion configs
     "hypr/hypridle.conf".source = ../hypr/hypridle.conf;
     "hypr/hyprlock.conf".source = ../hypr/hyprlock.conf;
     "hypr/hyprpaper.conf".source = ../hypr/hyprpaper.conf;
